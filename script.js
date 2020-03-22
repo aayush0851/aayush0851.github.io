55f6022dfea37750c1bd820a1b2d722d9cf5b0d2
aayush0851 <tyagiayush851@gmail.com>
2020-03-22 18:49:25 +0530

    final changes

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
-var x=1;
-
+var x=0;
+var arrayHead = ['Vacanza', 'Roll-da-dice', 'To-do App', 'Portfolio'];
+var arrayDes =['A basic HTML and CSS Webpage', 'A game based on Javascript', 'ReactJS based TO-DO App', ''];
 var slide_left_fwd = function(){
-    if(x>=3){
-        x=1;
+    if(x>=2){
+        x=0;
         document.getElementById('slide-image').src="/assets/slider/img"+x+".jpg";
+        document.getElementById('change-text-1').innerHTML=arrayHead[x];
+        document.getElementById('change-text-2').innerHTML=arrayDes[x];
     }
     else{
         x++;
         document.getElementById('slide-image').src="/assets/slider/img"+x+".jpg";
+        document.getElementById('change-text-1').innerHTML=arrayHead[x];
+        document.getElementById('change-text-2').innerHTML=arrayDes[x];
     }
 };
 var slide_right_bck = function(){
-    if(x>=3){
-        x=1;
+    if(x<=0){
+        x=2;
         document.getElementById('slide-image').src="/assets/slider/img"+x+".jpg";
+        document.getElementById('change-text-1').innerHTML=arrayHead[x];
+        document.getElementById('change-text-2').innerHTML=arrayDes[x];
     }
     else{
-        x++;
+        x--;
         document.getElementById('slide-image').src="/assets/slider/img"+x+".jpg";
+        document.getElementById('change-text-1').innerHTML=arrayHead[x];
+        document.getElementById('change-text-2').innerHTML=arrayDes[x];
     }
 };
 setInterval(slide_left_fwd, 3000);
+
+
+var close = function (){
+    document.getElementById('toggle-bar').style.width="0";
+    document.getElementById('toggle-bar').style.left=-500;
+}
+var open = function (){
+    document.getElementById('toggle-bar').style.width="100%";
+    document.getElementById('toggle-bar').style.left=0;
+}
 document.getElementById('fwd').addEventListener("click", slide_left_fwd);
 document.getElementById('bck').addEventListener("click", slide_right_bck);
+document.getElementById('hamburgerIcon').addEventListener("click", open);
+document.getElementById('cross').addEventListener("click", close);
+
+
+function toggle(){
+    document.getElementById('toggle-bar').style.width="0";
+    document.getElementById('toggle-bar').style.left=-500;
+}
